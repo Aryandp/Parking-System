a36c5cccbec9922801a8d589e95b1e470936c6a5
rajeev <rajeev23cusat@gmail.com>
2018-02-08 13:52:18 +0530

    html completed

--- scripts.js
+++ scripts.js
@@ -1,19 +1,18 @@
-
-
+var colors = ['black', 'white', 'blue', 'red'];
+// myDiv.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+$(document).ready(function () {
 
     var n = prompt("enter the no. parking");
     for (i = 1; i <= n; i++) {
         $("table").append("<tr id='slot" + i + "''><td id='reg" + i + "''>empty</td><td id='colour" + i + "''>" +
             "<button id='btn-col" + i + "'' type='button' class=' bt btn btn-danger'></button></td><td>" + i + "</td><td id='operation" + i + "''>" +
             "<button id='remove" + i + "' class='btn btn-danger btn-sm remove' name='" + i + "'>" +
-            "<span class='glyphicon glyphicoremove'></span>remove</button></td></tr>"
+            "<span class='glyphicon glyphicon'></span>remove</button></td></tr>"
         );
 
     }
 
     var m = prompt("enter no of cars available");
-    alert(n);
-    alert(m);
     if( parseInt(n) < parseInt(m))
     {
         alert("less parking capacity");
@@ -24,7 +23,9 @@
             var check;
             var col=colors[Math.floor(Math.random() * colors.length)];
             for (i = 1; i <= n; i++) {
-
+                var st1=Math.floor(10 + (99 - 10) * Math.random());
+                var st2=Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 2).toUpperCase();
+                var st3=Math.floor(1000 + (9999 - 1000) * Math.random());
                 $(".btn-col").css("background-color",col );
                 check = $("#reg" + i).html();
                 if (check == "empty") {
@@ -40,5 +41,50 @@
         }
     }
 
+    $("#allocateParkingSlot").click(function () {
+        var registrationNo = $("#registrationNo").val();
+        var check;
+        var count;
+        var i;
+        for (i = 1; i <= n; i++) {
+            check = $("#reg" + i).html();
+            if (check == "empty") {
+                $("#reg" + i).html(registrationNo);
+                break;
+
+            }
 
 
+
+
+        }
+        $("#registrationNo").val("");
+
+
+        if( parseInt(i) > parseInt(n))
+        {
+            alert("less parking capacity");
+        }
+
+
+    });
+
+    $(".remove").click(function () {
+        var remove = $(this).attr("name");
+        $("#reg" + remove).html("empty");
+        $("#btn-col" + remove).css("background-color","transparent" );
+    });
+
+
+    $(document).ready(function(){
+        $('a[href="#search"]').on('click', function(event) {
+            $('#search').addClass('open');
+            $('#search > form > input[type="search"]').focus();
+        });
+        $('#search, #search button.close').on('click keyup', function(event) {
+            if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
+                $(this).removeClass('open');
+            }
+        });
+    });
+});
